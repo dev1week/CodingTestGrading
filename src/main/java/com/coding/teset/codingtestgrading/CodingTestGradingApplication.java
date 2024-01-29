2f02fc4b0b045d7a63bf593425777c4bc0a0adee
dev1week <rondo2860@gmail.com>
2024-01-30 01:54:03 +0900

    Initial commit

---
+++ src/main/java/com/coding/teset/codingtestgrading/CodingTestGradingApplication.java
@@ -0,0 +1,13 @@
+package com.coding.teset.codingtestgrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestGradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestGradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coding/teset/codingtestgrading/CodingTestGradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.teset.codingtestgrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestGradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
